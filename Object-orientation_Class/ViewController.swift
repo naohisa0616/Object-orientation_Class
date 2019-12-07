4329993ecaf05adbcee04102954b83a4ec778094
naohisa0616 <naohisa0616@icloud.com>
2019-12-07 19:58:43 +0900

    手順 ① MyClassクラスの定義 ② MyClassクラス定義を追加したViewController.swift ②MyClassクラスのインスタンスmyObjを作る

--- Object-orientation_Class/ViewController.swift
+++ Object-orientation_Class/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
+//書式　プロパティとメソッドがあるクラス定義
+//class クラス名 {
+//    インスタンスプロパティ（付帯する情報）（プロパティは定数、変数で宣言する）（メンバ変数）
+//   let 定数名:型 = 値
+//   var 変数名:型 = 値
+    
+//    インスタンスメソッド（メソッドは関数で定義します）（機能をメソッド定義）（オブジェクトの操作を定義するもの）（動きとしては、関数と同じ）
+//    func メソッド名(引数名:型) -> 戻り値の型 {
+//        ステートメント
+//        return 戻り値
+//    }
+//}
+//
+
+//MyClassクラスの定義
+class MyClass {
+//    インスタンスプロパティ
+    let msg = "ハロー"
+//    インスタンスメソッド
+    func hello()  {
+        print(msg)
+    }
+    
+}
+
+//ViewControllerクラス定義
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//      MyClassクラスのインスタンスmyObjを作る
+//      MyClassクラスのインスタンスを作って変数myObjに入れます
+        let myObj = MyClass()
+//      hello()メソッドを実行する
+        myObj.hello()
     }
 
 
 }
+
+
 
