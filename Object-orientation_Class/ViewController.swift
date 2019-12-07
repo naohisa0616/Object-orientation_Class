43e11309490459bd83d1979ad71356cf94cb4122
naohisa0616 <naohisa0616@icloud.com>
2019-12-07 17:07:49 +0900

    Initial Commit

---
+++ Object-orientation_Class/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Object-orientation_Class
+//
+//  Created by 宮崎直久 on 2019/12/07.
+//  Copyright © 2019 宮崎直久. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
